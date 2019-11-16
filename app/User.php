3247e2204802ba8c6c8aaa23d95f25cba7ce41ed
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2019-11-16 14:15:36 +0300

    Added Role And Permission Models.

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Zizaco\Entrust\Traits\EntrustUserTrait;
 
 class User extends Authenticatable
 {

d14e1d7d8b618de778ce4b78b88176e7d9ec81a7
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2019-12-19 17:50:28 +0300

    namespace changes..

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,7 @@ Route::get('/forbidden', function () {
     return view('forbidden');
 });
 
+
 //! submission of kpi scores 
 
 Route::post('submitScores','userControllers\UserController@submittingKPIScores');

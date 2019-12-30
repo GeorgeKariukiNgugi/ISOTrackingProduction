b7ca5098fb07098c4aa4e0d8cd01167c2660c4a8
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2019-12-30 20:32:35 +0300

    Grouped bar chart implementation.

--- app/Http/Controllers/adminController/trends.php
+++ app/Http/Controllers/adminController/trends.php
@@ -19,6 +19,9 @@ class trends extends Controller
         $data4 = [0,18,44];
         $data5 = [0,17,19];
         $data6 = [0,00,50];
+        // $trialCharts->displaylegend(true);
+        // $trialCharts->displayAxes(true, true);
+        // $trialCharts->title('Users by Months', 30, "rgb(255, 99, 132)", true, 'Helvetica Neue');
         $trialCharts->labels(['2019 Q1', '2019 Q2', '2019 Q3']);
         $trialCharts->dataset('ISMS', 'line', $data1)->fill(false)->color("rgb(255, 99, 132)");
         $trialCharts->dataset('ITSMS', 'line', $data2)->fill(false)->color("rgb(0, 0, 255)");
@@ -29,7 +32,78 @@ class trends extends Controller
         // $trialCharts->data(
         //     dataset
         // )
+
+        //! this is the code for the grouped bar charts. 
+        $borderColors = [
+            "rgba(255, 99, 132, 1.0)",
+            "rgba(22,160,133, 1.0)",
+            "rgba(255, 205, 86, 1.0)",
+            "rgba(51,105,232, 1.0)",
+            "rgba(244,67,54, 1.0)",
+            "rgba(34,198,246, 1.0)",
+            "rgba(153, 102, 255, 1.0)",
+            "rgba(255, 159, 64, 1.0)",
+            "rgba(233,30,99, 1.0)",
+            "rgba(205,220,57, 1.0)"
+        ];
+        $fillColors = [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(22,160,133, 0.2)",
+            "rgba(255, 205, 86, 0.2)",
+            "rgba(51,105,232, 0.2)",
+            "rgba(244,67,54, 0.2)",
+            "rgba(34,198,246, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+            "rgba(233,30,99, 0.2)",
+            "rgba(205,220,57, 0.2)"
+
+        ];
+
+        $borderColors2 = [
+            // "rgba(255, 99, 132, 1.0)",
+            // "rgba(22,160,133, 1.0)",
+            // "rgba(255, 205, 86, 1.0)",
+            // "rgba(51,105,232, 1.0)",
+            // "rgba(244,67,54, 1.0)",
+            // "rgba(34,198,246, 1.0)",
+            "rgba(153, 102, 255, 1.0)",
+            "rgba(255, 159, 64, 1.0)",
+            "rgba(233,30,99, 1.0)",
+            "rgba(205,220,57, 1.0)"
+        ];
+        $fillColors2 = [
+            // "rgba(255, 99, 132, 0.2)",
+            // "rgba(22,160,133, 0.2)",
+            // "rgba(255, 205, 86, 0.2)",
+            // "rgba(51,105,232, 0.2)",
+            // "rgba(244,67,54, 0.2)",
+            // "rgba(34,198,246, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+            "rgba(233,30,99, 0.2)",
+            "rgba(205,220,57, 0.2)"
+
+        ];
+
+        $usersChart = new DashBoardCharts;
+        $usersChart->minimalist(false);
+        $usersChart->labels(['Jan', 'Feb', 'Mar']);
+
+        $usersChart->dataset('ITSMS', 'bar', [10, 25, 13])
+            ->color($borderColors)
+            // ->label("First")
+            ->backgroundcolor($fillColors);
+
+        $usersChart->dataset('ISMS', 'bar', [20, 50, 26])
+            ->color($borderColors2)
+            ->backgroundcolor($fillColors2);
+
+        $usersChart->dataset('QMS', 'bar', [30, 75, 39])
+            ->color($borderColors)
+            ->backgroundcolor($fillColors);
+
         $programs = Program::all();
-        return view('adminPage.trends',['trialCharts'=>$trialCharts,'programs'=>$programs]);
+        return view('adminPage.trends',['trialCharts'=>$trialCharts,'programs'=>$programs,'usersChart'=>$usersChart]);
     }
 }
--- resources/views/adminPage/trends.blade.php
+++ resources/views/adminPage/trends.blade.php
@@ -1,13 +1,15 @@
 @extends('extendingCode.adminExtending')
 @section('charts')
 {!! $trialCharts->script() !!}
+{!! $usersChart->script() !!}
+
 {{-- {!! $ncsperProgramCharts->script() !!} --}}
 
 @endsection
 @section('section')
 
 <div class="row" style="">
-    <div class="col-md-10 col-md-offset-1">
+    <div class="col-md-6">
       <div class="box box-danger">
           <div class="box-header with-border text-center" >
             <h3 class="box-title"> <span style="font-family:Georgia, 'Times New Roman', Times, serif">TRENDS PER PROGRAM PER QUATER PER YEAR.</span></h3>
@@ -18,6 +20,17 @@
           <!-- /.box-body -->
         </div>
   </div> 
+  <div class="col-md-6">
+    <div class="box box-danger">
+        <div class="box-header with-border text-center" >
+          <h3 class="box-title"> <span style="font-family:Georgia, 'Times New Roman', Times, serif">TRENDS PER PROGRAM PER QUATER PER YEAR.</span></h3>
+        </div>
+        <div class="box-body">
+                {!!$usersChart->container()!!}
+        </div>
+        <!-- /.box-body -->
+      </div>
+</div> 
 </div>
 
 

a86938c42eeb85f6f2ed042d0bc1c5524cf12c79
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2019-12-31 10:22:28 +0300

    Implemented grouped bar charts.

--- app/Http/Controllers/adminController/trends.php
+++ app/Http/Controllers/adminController/trends.php
@@ -22,6 +22,7 @@ class trends extends Controller
         // $trialCharts->displaylegend(true);
         // $trialCharts->displayAxes(true, true);
         // $trialCharts->title('Users by Months', 30, "rgb(255, 99, 132)", true, 'Helvetica Neue');
+        $trialCharts->height(550);
         $trialCharts->labels(['2019 Q1', '2019 Q2', '2019 Q3']);
         $trialCharts->dataset('ISMS', 'line', $data1)->fill(false)->color("rgb(255, 99, 132)");
         $trialCharts->dataset('ITSMS', 'line', $data2)->fill(false)->color("rgb(0, 0, 255)");
@@ -61,24 +62,24 @@ class trends extends Controller
         ];
 
         $borderColors2 = [
-            // "rgba(255, 99, 132, 1.0)",
-            // "rgba(22,160,133, 1.0)",
-            // "rgba(255, 205, 86, 1.0)",
-            // "rgba(51,105,232, 1.0)",
-            // "rgba(244,67,54, 1.0)",
-            // "rgba(34,198,246, 1.0)",
+            "rgba(255, 99, 132, 1.0)",
+            "rgba(22,160,133, 1.0)",
+            "rgba(255, 205, 86, 1.0)",
+            "rgba(51,105,232, 1.0)",
+            "rgba(244,67,54, 1.0)",
+            "rgba(34,198,246, 1.0)",
             "rgba(153, 102, 255, 1.0)",
             "rgba(255, 159, 64, 1.0)",
             "rgba(233,30,99, 1.0)",
             "rgba(205,220,57, 1.0)"
         ];
         $fillColors2 = [
-            // "rgba(255, 99, 132, 0.2)",
-            // "rgba(22,160,133, 0.2)",
-            // "rgba(255, 205, 86, 0.2)",
-            // "rgba(51,105,232, 0.2)",
-            // "rgba(244,67,54, 0.2)",
-            // "rgba(34,198,246, 0.2)",
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(22,160,133, 0.2)",
+            "rgba(255, 205, 86, 0.2)",
+            "rgba(51,105,232, 0.2)",
+            "rgba(244,67,54, 0.2)",
+            "rgba(34,198,246, 0.2)",
             "rgba(153, 102, 255, 0.2)",
             "rgba(255, 159, 64, 0.2)",
             "rgba(233,30,99, 0.2)",
@@ -88,20 +89,25 @@ class trends extends Controller
 
         $usersChart = new DashBoardCharts;
         $usersChart->minimalist(false);
-        $usersChart->labels(['Jan', 'Feb', 'Mar']);
+        $usersChart->height(550);
+        $usersChart->labels(['ITSMS 2019/2020 Quaterly Trends.', 'ISMS 2019/2020 Quaterly Trends.', 'QMS 2019/2020 Quaterly Trends.','EMS 2019/2020 Quaterly Trends.','CSR 2019/2020 Quaterly Trends.','BCMS 2019/2020 Quaterly Trends.']);
+        // $usersChart->labels(['Jan1', 'Feb1', 'Mar1']);
 
-        $usersChart->dataset('ITSMS', 'bar', [10, 25, 13])
-            ->color($borderColors)
-            // ->label("First")
-            ->backgroundcolor($fillColors);
+        $usersChart->dataset('Q1','bar', [10, 25, 13,42,23,45])
+            ->color($borderColors2[0])
+            ->backgroundcolor($fillColors2[0]);
 
-        $usersChart->dataset('ISMS', 'bar', [20, 50, 26])
-            ->color($borderColors2)
-            ->backgroundcolor($fillColors2);
+        $usersChart->dataset('Q2','bar', [20, 50, 26,33,53,66])
+            ->color($borderColors2[1])
+            ->backgroundcolor($fillColors2[1]);
 
-        $usersChart->dataset('QMS', 'bar', [30, 75, 39])
-            ->color($borderColors)
-            ->backgroundcolor($fillColors);
+        $usersChart->dataset('Q3','bar', [30, 75, 39,28,34,45])
+            ->color($borderColors[2])
+            ->backgroundcolor($fillColors[2]);
+
+        $usersChart->dataset('Q4','bar', [30, 75, 39,14,45,12])
+            ->color($borderColors[3])
+            ->backgroundcolor($fillColors[3]);       
 
         $programs = Program::all();
         return view('adminPage.trends',['trialCharts'=>$trialCharts,'programs'=>$programs,'usersChart'=>$usersChart]);
--- resources/views/adminPage/trends.blade.php
+++ resources/views/adminPage/trends.blade.php
@@ -7,20 +7,25 @@
 
 @endsection
 @section('section')
-
-<div class="row" style="">
-    <div class="col-md-6">
-      <div class="box box-danger">
-          <div class="box-header with-border text-center" >
-            <h3 class="box-title"> <span style="font-family:Georgia, 'Times New Roman', Times, serif">TRENDS PER PROGRAM PER QUATER PER YEAR.</span></h3>
-          </div>
-          <div class="box-body">
-                  {!!$trialCharts->container()!!}
-          </div>
-          <!-- /.box-body -->
-        </div>
-  </div> 
-  <div class="col-md-6">
+<hr>
+<h2 style="text-align:center;font-family:'Times New Roman', Times, serif;font-size:42px;color:#72BBE9"> <i class="fa fa-line-chart"></i> TRENDS TAB.</h2>
+<hr>
+<div class="col-md-4 col-md-offset-4">
+  <h3 style="text-align:left;font-family:'Times New Roman', Times, serif;">Trends Table Of Contents.</h3>
+  <ol style="text-align:left;font-family:'Times New Roman', Times, serif;">
+    <li>
+      <a href=""> Line Graph Trends For All Programs.</a>
+    </li>
+    <li>
+      <a href=""> Grouped Bar Chart 2019 Quaterly Trends Per Programs</a>
+    </li>
+    <li>
+      <a href=""> Grouped Bar Chart For </a>
+    </li>
+  </ol>
+</div>
+<div class="row">
+  <div class="col-md-12">
     <div class="box box-danger">
         <div class="box-header with-border text-center" >
           <h3 class="box-title"> <span style="font-family:Georgia, 'Times New Roman', Times, serif">TRENDS PER PROGRAM PER QUATER PER YEAR.</span></h3>
@@ -32,6 +37,20 @@
       </div>
 </div> 
 </div>
-
+<div class="row" style="">
+  <div class="col-md-10 col-md-offset-1">
+    <div class="box box-danger">
+        <div class="box-header with-border text-center" >
+          <h3 class="box-title"> <span style="font-family:Georgia, 'Times New Roman', Times, serif">TRENDS PER PROGRAM PER QUATER PER YEAR.</span></h3>
+        </div>
+        <p style="color:red;text-align:center;">This greph is not giving a full picture:</p>
+        <div class="box-body">
+          
+                {!!$trialCharts->container()!!}
+        </div>
+        <!-- /.box-body -->
+      </div>
+</div>   
+</div>
 
 @endsection

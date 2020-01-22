67b8964584b07c90d497b1b65ab7ca125306491f
George Kariuki Ngugi <gkngugi@safaricom.co.ke>
2020-01-22 09:23:52 +0300

    more changes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -90,7 +90,7 @@ class LoginController extends Controller
                 ldap_close($ds);
                 // return $res['fullname'];
 
-                return redirect()->route('/home');
+                return redirect('/home');
 
             }
             else

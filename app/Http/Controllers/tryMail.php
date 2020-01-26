94fec21066e589e415247093d169342c1558cdab
George Kariuki Ngugi <gkngugi@safaricom.co.ke>
2020-01-26 19:56:30 +0300

    Added all changes

--- app/Http/Controllers/tryMail.php
+++ app/Http/Controllers/tryMail.php
@@ -8,18 +8,5 @@ use App\Mail\TestMail;
 class tryMail extends Controller
 {
     public function trySendingMial(){
-
-        try {
-            //code...
-            Mail::to('gkngugi@safaricom.co.ke')->send(new TestMail)->getTransport()->stop();
-            die('mail sent!');
-        return "sent.";
-        } catch (\Throwable $th) {
-            //throw $th;
-            return "Error Thrown.";
-        }
-
-        
-
     }
 }

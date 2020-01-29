e7e17d4c90cf844cb84085be4d88ac2c4ef72771
George Kariuki Ngugi <gkngugi@safaricom.co.ke>
2020-01-29 14:25:29 +0300

    This is the trial message

--- resources/views/email/testemail.blade.php
+++ resources/views/email/testemail.blade.php
@@ -18,6 +18,27 @@ This is the panel content.
 | Col 3 is      | Right-Aligned | $20      |
 @endcomponent
 
+<div class="table-responsive">
+    <table class="table table-striped table-bordered table-condensed">
+        <thead>
+            <tr>
+                <th>Column 1</th>
+                <th>Column 2</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Cell 1</td>
+                <td>Cell 2</td>
+            </tr>
+            <tr>
+                <td>Cell 3</td>
+                <td>Cell 4</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
 Thanks,<br>
 {{ config('app.name') }}
 @endcomponent

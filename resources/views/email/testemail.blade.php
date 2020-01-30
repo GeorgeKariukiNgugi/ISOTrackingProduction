4dd0551a4700790719ea05fb577f9e04a38bd395
root <root@svdt5isoscard.safaricom.net>
2020-01-30 17:24:05 +0300

    This is the message

--- resources/views/email/testemail.blade.php
+++ resources/views/email/testemail.blade.php
@@ -12,32 +12,12 @@ This is the panel content.
 @endcomponent
 
 @component('mail::table')
-| Laravel       | Table         | Example  |
-| ------------- |:-------------:| --------:|
-| Col 2 is      | Centered      | $10      |
-| Col 3 is      | Right-Aligned | $20      |
+first	first1
+second	first2
+third	first3
+forth	first4
+fifth	first5
 @endcomponent
-
-<div class="table-responsive">
-    <table class="table table-striped table-bordered table-condensed">
-        <thead>
-            <tr>
-                <th>Column 1</th>
-                <th>Column 2</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Cell 1</td>
-                <td>Cell 2</td>
-            </tr>
-            <tr>
-                <td>Cell 3</td>
-                <td>Cell 4</td>
-            </tr>
-        </tbody>
-    </table>
-</div>
 
 Thanks,<br>
 {{ config('app.name') }}

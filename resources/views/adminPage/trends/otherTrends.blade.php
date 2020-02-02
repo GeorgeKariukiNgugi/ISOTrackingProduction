328c5db05c98254bb9dd436aca6df4e53f555beb
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-02-02 21:34:37 +0300

    Changes added

--- resources/views/adminPage/trends/otherTrends.blade.php
+++ resources/views/adminPage/trends/otherTrends.blade.php
@@ -59,16 +59,26 @@
                 <h3 class="box-title"> <span style="font-size:28px;font-family:Georgia, 'Times New Roman', Times, serif">{{ strtoupper($nameOfDoughnutChart) .'  '}}</span> <span><b style="font-size:28px;font-family:Verdana, Geneva, Tahoma, sans-serif">
                   {{-- {{ sprintf("%.2f", $average[$start])."%"}} --}}
 
-                  @for ($i = 0; $i < count($scoresInPercentages); $i++)
+                  @php
+                  //! this section of the code is used to et the general scores that are in the form of numbers. 
+                  $scoresInPercentageIndex = $scoresInPercentages[$start];
+                  $scoresInPercentagesNamesIndex = $scoresInPercentagesNames[$start];
+                  $score = 0;
+                  // dd($scoresInPercentageIndex);
+
+
+              @endphp
+              <p style="text-align:center;">
+              @for ($i = 0; $i < count($scoresInPercentageIndex); $i++)
 
                   @php
-                    $score += round($scoresInPercentages[$i],2) 
+                      $score += round($scoresInPercentageIndex[$i],2)
                   @endphp
-                  {{-- {{}}</b></span> </span> --}}
+                  
                   {{-- {{$scoresInPercentagesNamesIndex[$i] . '  '. round($scoresInPercentageIndex[$i],2)}}                   --}}
                   
               @endfor
-                {{$score}}
+              {{$score}}
                 </b></span></h3>
               </div>
               <p style="color:red;font-size:13px;font-style:italic;"> <span> <b>Disclaimer</b> If The Sectors are less than 1% then the perspective has not been assesed for that quater.</span> </p>

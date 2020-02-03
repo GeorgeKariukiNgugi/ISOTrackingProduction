c4608b3fc90788954924950597a0a4db84d8c496
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-02-03 07:42:26 +0300

    message]

--- resources/views/email/testemail.blade.php
+++ resources/views/email/testemail.blade.php
@@ -1,29 +1,10 @@
-@component('mail::message')
-# Introduction
-
-The body of your message.
-
-@component('mail::button', ['url' => '/'])
-Button Text
-@endcomponent
-
-@foreach ($nonConformities as $$nonConformities)
+@foreach ($nonConformities as $nonConformities)
 
 {{$nonConformities->date}}
-    
+    @if ($nonConformities->date == null)
+        {{"nodate"}}
+    @else
+        {{"present dara"}}
+    @endif
 @endforeach
-@component('mail::panel')
-This is the panel content.
-@endcomponent
 
-@component('mail::table')
-first	first1
-second	first2
-third	first3
-forth	first4
-fifth	first5
-@endcomponent
-
-Thanks,<br>
-{{ config('app.name') }}
-@endcomponent

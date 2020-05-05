ce9cd30dcc1f85273b4a74ccf9d1afc8d1395bbd
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-05-05 09:20:15 -0700

    Added more changes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -307,149 +307,145 @@ class LoginController extends Controller
                                         
     public function logInUsingLDAP(Request $request){
         
-        // This section of the code is used as a debugging tool so as to see if the LDAP server is working. 
+        // // This section of the code is used as a debugging tool so as to see if the LDAP server is working. 
 
-        // Getting the logIn Credentials. 
+        // // Getting the logIn Credentials. 
 
-        Session::put('name', $request->email);
-        Session::put('email', $request->email);
+        // Session::put('name', $request->email);
+        // Session::put('email', $request->email);
 
-        $programids = AssesorPerProgram::where('email','=',$request->email)->get();
-        $countingid = count($programids);
-        $id = "null";
-        if($countingid == 1){
-            foreach($programids as $programid){
-                //!getting the program that the user should asses, and all its perspetives.
-                $id = $programid->program_id;
+        // $programids = AssesorPerProgram::where('email','=',$request->email)->get();
+        // $countingid = count($programids);
+        // $id = "null";
+        // if($countingid == 1){
+        //     foreach($programids as $programid){
+        //         //!getting the program that the user should asses, and all its perspetives.
+        //         $id = $programid->program_id;
                 
-            }
-            return $this->redirectTo($id);
-        }
-        else if($countingid > 1){                 
-         //    return "You Have more Than One Accounts to update.";
-         //! this is the array that is used to hold the array of short hands. 
-         $programShortHand = array();
-         $programId = array();
-         foreach ($programids as $programid) {
-             # code...
-             if($programid->program_id == 0){
-                 array_push($programShortHand,'Admin');
-                 array_push($programId,$programid->program_id);
-             }
-             else{
-                 $programWithIds = Program::where('id','=',$programid->program_id)
-                 ->get();
+        //     }
+        //     return $this->redirectTo($id);
+        // }
+        // else if($countingid > 1){                 
+        //  //    return "You Have more Than One Accounts to update.";
+        //  //! this is the array that is used to hold the array of short hands. 
+        //  $programShortHand = array();
+        //  $programId = array();
+        //  foreach ($programids as $programid) {
+        //      # code...
+        //      if($programid->program_id == 0){
+        //          array_push($programShortHand,'Admin');
+        //          array_push($programId,$programid->program_id);
+        //      }
+        //      else{
+        //          $programWithIds = Program::where('id','=',$programid->program_id)
+        //          ->get();
 
-                 foreach($programWithIds as $programWithId){
-                     array_push($programShortHand,$programWithId->shortHand);
-                     array_push($programId,$programid->program_id);
-                 }
-             }
-         }                        
-         return view('userWithMoreThanOneProgram',['programShortHand'=>$programShortHand,'programId'=>$programId]);
-        }
-        else{
-         return view('forbidden');
-        }   
-
-
-
-        
-        
-        // header('Content-type: application/json');
-        // $_JSON = json_decode(file_get_contents('php://input'), true);
-        // $username = str_replace('@safaricom.co.ke', '', strip_tags($request->email));
-        // $password = strip_tags($request->password);
-        // $ldap_svr = "172.28.229.103";
-        // $ldap_port = "389";
-        // $ldapuser = $username."@safaricom.net";
-        // $uname = "SAFARICOM\\".$username;
-        // $dn = "ou=safaricom departments,dc=safaricom,dc=net";
-        // $pieces = explode("\\", $uname);
-        // $uname1 = $pieces[1];
-        // $ds = ldap_connect($ldap_svr, $ldap_port);
+        //          foreach($programWithIds as $programWithId){
+        //              array_push($programShortHand,$programWithId->shortHand);
+        //              array_push($programId,$programid->program_id);
+        //          }
+        //      }
+        //  }                        
+        //  return view('userWithMoreThanOneProgram',['programShortHand'=>$programShortHand,'programId'=>$programId]);
+        // }
+        // else{
+        //  return view('forbidden');
+        // }   
+                
+        header('Content-type: application/json');
+        $_JSON = json_decode(file_get_contents('php://input'), true);
+        $username = str_replace('@safaricom.co.ke', '', strip_tags($request->email));
+        $password = strip_tags($request->password);
+        $ldap_svr = "172.28.229.103";
+        $ldap_port = "389";
+        $ldapuser = $username."@safaricom.net";
+        $uname = "SAFARICOM\\".$username;
+        $dn = "ou=safaricom departments,dc=safaricom,dc=net";
+        $pieces = explode("\\", $uname);
+        $uname1 = $pieces[1];
+        $ds = ldap_connect($ldap_svr, $ldap_port);
     
-        // if ($ds)
-        // {
-        //     ldap_set_option($ds, LDAP_OPT_REFERRALS, 0);
-        //     ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3);
-        //     $ldapbind = @ldap_bind($ds, $ldapuser, $password);
+        if ($ds)
+        {
+            ldap_set_option($ds, LDAP_OPT_REFERRALS, 0);
+            ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3);
+            $ldapbind = @ldap_bind($ds, $ldapuser, $password);
     
-        //     if ($ldapbind)
-        //     {
-        //         $filter = "(sAMAccountName=" . $username . ")";
-        //         $attr = array("memberof", "givenname", "mail", "cn", "sn", "samaccountname");
-        //         $result = ldap_search($ds, $dn, $filter, $attr) or exit("Unable to search LDAP server");
-        //         $entries = ldap_get_entries($ds, $result);
-        //         $givenname = $entries[0]['givenname'][0];
+            if ($ldapbind)
+            {
+                $filter = "(sAMAccountName=" . $username . ")";
+                $attr = array("memberof", "givenname", "mail", "cn", "sn", "samaccountname");
+                $result = ldap_search($ds, $dn, $filter, $attr) or exit("Unable to search LDAP server");
+                $entries = ldap_get_entries($ds, $result);
+                $givenname = $entries[0]['givenname'][0];
     
-        //         $i = 0;
-        //         $res = [
-        //             'fullname' => $entries[0]["cn"][0],
-        //             'givenname' => $entries[0]["givenname"][0],
-        //             'surname' => $entries[0]["sn"][0],
-        //             'username' => strtolower($entries[0]["samaccountname"][0]),
-        //             'email' => $entries[0]["mail"][0]
-        //         ];                               
-        //         ldap_close($ds);                
-        //         Session::put('name', $res['fullname'] );
-        //         Session::put('email', $res['email'] );
+                $i = 0;
+                $res = [
+                    'fullname' => $entries[0]["cn"][0],
+                    'givenname' => $entries[0]["givenname"][0],
+                    'surname' => $entries[0]["sn"][0],
+                    'username' => strtolower($entries[0]["samaccountname"][0]),
+                    'email' => $entries[0]["mail"][0]
+                ];                               
+                ldap_close($ds);                
+                Session::put('name', $res['fullname'] );
+                Session::put('email', $res['email'] );
 
-        //         // echo $loggedInemail;
-        //         //! add the data to redirect a user. 
-        //         $programids = AssesorPerProgram::where('email','=',Session::get('email'))->get();
-        //         $countingid = count($programids);
-        //         $id = "null";
-        //         if($countingid == 1){
-        //             foreach($programids as $programid){
-        //                 //!getting the program that the user should asses, and all its perspetives.
-        //                 $id = $programid->program_id;
+                // echo $loggedInemail;
+                //! add the data to redirect a user. 
+                $programids = AssesorPerProgram::where('email','=',Session::get('email'))->get();
+                $countingid = count($programids);
+                $id = "null";
+                if($countingid == 1){
+                    foreach($programids as $programid){
+                        //!getting the program that the user should asses, and all its perspetives.
+                        $id = $programid->program_id;
                         
-        //             }
-        //             return $this->redirectTo($id);
-        //         }
-        //         else if($countingid > 1){                 
-        //          //    return "You Have more Than One Accounts to update.";
-        //          //! this is the array that is used to hold the array of short hands. 
-        //          $programShortHand = array();
-        //          $programId = array();
-        //          foreach ($programids as $programid) {
-        //              # code...
-        //              if($programid->program_id == 0){
-        //                  array_push($programShortHand,'Admin');
-        //                  array_push($programId,$programid->program_id);
-        //              }
-        //              else{
-        //                  $programWithIds = Program::where('id','=',$programid->program_id)
-        //                  ->get();
+                    }
+                    return $this->redirectTo($id);
+                }
+                else if($countingid > 1){                 
+                 //    return "You Have more Than One Accounts to update.";
+                 //! this is the array that is used to hold the array of short hands. 
+                 $programShortHand = array();
+                 $programId = array();
+                 foreach ($programids as $programid) {
+                     # code...
+                     if($programid->program_id == 0){
+                         array_push($programShortHand,'Admin');
+                         array_push($programId,$programid->program_id);
+                     }
+                     else{
+                         $programWithIds = Program::where('id','=',$programid->program_id)
+                         ->get();
 
-        //                  foreach($programWithIds as $programWithId){
-        //                      array_push($programShortHand,$programWithId->shortHand);
-        //                      array_push($programId,$programid->program_id);
-        //                  }
-        //              }
-        //          }                        
-        //          return view('userWithMoreThanOneProgram',['programShortHand'=>$programShortHand,'programId'=>$programId]);
-        //         }
-        //         else{
-        //          return view('forbidden');
-        //         }      
+                         foreach($programWithIds as $programWithId){
+                             array_push($programShortHand,$programWithId->shortHand);
+                             array_push($programId,$programid->program_id);
+                         }
+                     }
+                 }                        
+                 return view('userWithMoreThanOneProgram',['programShortHand'=>$programShortHand,'programId'=>$programId]);
+                }
+                else{
+                 return view('forbidden');
+                }      
                 
 
-        //     }
-        //     else
-        //     {
-        //         http_response_code(401); 
-        //         # Unauthorized
-        //         return back()->withErrors(["The Credentials Don't Match Our Records"]);
-        //         echo "Invalid credentials. Please try again.";
-        //     }
-        // }
-        // else
-        // {
-        //     http_response_code(503); # Service unavailable
-        //     //echo "Unable to connect to LDAP server";
-        // }
+            }
+            else
+            {
+                http_response_code(401); 
+                # Unauthorized
+                return back()->withErrors(["The Credentials Don't Match Our Records"]);
+                echo "Invalid credentials. Please try again.";
+            }
+        }
+        else
+        {
+            http_response_code(503); # Service unavailable
+            //echo "Unable to connect to LDAP server";
+        }
     
     }
     /**
